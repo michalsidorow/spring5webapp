8a51e069f9ca00b105a732aad48e6593e15f2e02
michalsidorow <sztucznymail123@wp.pl>
2020-03-30 18:15:52 +0200

    Initializing project

--- src/test/java/guru/springframework/wetapp/WetApplicationTests.java
+++ src/test/java/guru/springframework/wetapp/WetApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring5webapp;
+package guru.springframework.wetapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Spring5webappApplicationTests {
+public class WetApplicationTests {
 
 	@Test
 	public void contextLoads() {
